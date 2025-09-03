ed851d0ea1717f42ed96ebfa5d3b5bfe6be7a1f9
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2025-09-03 23:33:14 +0530

    coash-test

--- src/Main/Coaches/CoachProfile.jsx
+++ src/Main/Coaches/CoachProfile.jsx
@@ -45,7 +45,6 @@ export default function CoachesProfile() {
 
   // Verification function example
   const verify = () => {
-    alert("Verifying...");
     const endpoint =
       information.verified === false
         ? "/coach/verify-coach"
@@ -55,12 +54,9 @@ export default function CoachesProfile() {
 
     if (!endpoint) return;
 
-    alert("Verifying...");
-
     axios
       .post(data.url + endpoint, { id }, { withCredentials: true })
       .then((res) => {
-        console.log(res.data);
         if (res.data.alert) {
           alert(res.data.alert);
         } else {
@@ -76,7 +72,6 @@ export default function CoachesProfile() {
               else setInformation(res.data.supply);
             });
         }
-        alert("Verification process completed.");
       });
   };
 
